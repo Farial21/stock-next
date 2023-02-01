450000a38eb044ace373a5b0f85a43e4a305db7d
Farial Azmain <farialazmain@gmail.com>
2023-02-01 15:50:07 +0700

    Added About Page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Link from 'next/Link'
+
+export default function AboutPage() {
+  return (
+    <>
+    <Head>
+      <title>About Page</title>
+    </Head>
+    <h1>About Page</h1>
+    <p>About Farial</p> 
+    <Link href= '/index'>Home</Link> 
+     
+    </>
+  )
+}
